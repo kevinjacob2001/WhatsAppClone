ea91d25af786df325da3173e2ca2d3b5b5b479b1
kevinjacob2001 <kevinjacob2001@gmail.com>
2021-03-26 14:11:15 +0530

    added header and btn

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,9 @@
-import {Avatar} from '@material-ui/core'
+import {Avatar, IconButton} from '@material-ui/core'
 import styled from 'styled-components'
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search'
+import {Button} from '@material-ui/core'
 
 function Sidebar() {
     return (
@@ -7,9 +11,20 @@ function Sidebar() {
             <Header>
                 <UserAvatar/>
                 <IconsContainer>
-
+                    <IconButton>
+                       <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                       <MoreVertIcon/>
+                    </IconButton>          
                 </IconsContainer>
             </Header >
+
+            <Search>
+                <SearchIcon/>
+                <SearchInput placeholder="Search in chat"/>
+            </Search>
+            <SidebarButton>Start a new chat</SidebarButton>
         </Container>
     )
 }
@@ -19,8 +34,45 @@ export default Sidebar
 
 const Container=styled.div``;
 
-const Header=styled.div``;
+const Search=styled.div`
+display:flex;
+align-items:center;
+padding:20px;
+border-radius:2px;
+`;
 
-const UserAvatar=styled(Avatar)``;
+const SearchInput=styled.input`
+ outline-width:0;
+ border:none;
+ flex:1;
+`;
+
+const SidebarButton=styled(Button)`
+ width:100%;
+border-top:1px solid whitesmoke;
+border-bottom:1px solid whitesmoke;
+ `;
+
+const Header=styled.div`
+    display:flex;
+    position:sticky;
+    top: 0;
+    background:white;
+    z-index: 1;
+    justify-content:space-between;
+    align-items:center;
+    padding:15px;
+    height:80px;
+    border-bottom:3px solid whitesmoke;
+`;
+
+
+
+const UserAvatar=styled(Avatar)`
+    cursor: pointer;
+        :hover{
+            opacity:0.8;
+        }
+`;
 
 const IconsContainer=styled.div``;
