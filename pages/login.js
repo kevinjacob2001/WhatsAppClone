19725629f69139734c05dbaeed77d4b8ac13db5b
kevinjacob2001 <kevinjacob2001@gmail.com>
2021-03-26 15:37:56 +0530

    added login page bg

--- pages/login.js
+++ pages/login.js
@@ -1,34 +1,62 @@
-import { Button } from '@material-ui/core';
+
 import Head from 'next/head';
 import styled from 'styled-components'
 
 function login() {
     return (
-        <Container>
+        <OuterContainer>
+            <Container>
             <Head>
                 <title>Login</title>
             </Head>
             <LoginContainer>
                 <Logo
                 src="https://i.pinimg.com/originals/f7/5d/94/f75d94874d855a7fcfcc922d89ac5e80.png"
-                width="140px"
-                />
-                <Button>Sign in with Google</Button>
+                    />
+                <Button variant="outlined">Sign in with Google</Button>
             </LoginContainer>
         </Container>
+        </OuterContainer>
     )
 }
 
 export default login
 
-const Container=styled.div`
+const OuterContainer=styled.div`
+height:100vh;
+width:100vw;
+background-image:url('https://wallpapercave.com/wp/wp1843761.jpg');
+background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
 
+const Container=styled.div`
+  position:absolute;
+  transform:translate(-50%,-50%);
+  top:50%;
+  left:50%;
 `;
 
 const LoginContainer=styled.div`
-
+  display:flex;
+  flex-direction:column
 `;
 
 const Logo=styled.img`
+   height:9rem;
+   width:13rem;
+   margin-bottom:3rem;
+`;
 
+const Button=styled.button`
+
+padding:1rem 2rem 1rem 2rem;
+border-radius:.7rem;
+border:none;
+outline-width:0;
+ :hover{
+     cursor:pointer;
+     background-color:#2aeae0;
+ }
 `;
